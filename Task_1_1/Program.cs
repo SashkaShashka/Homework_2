e6a7a46fbe116e2e48ac56a6b60cf7906b16ea55
SashkaShashka <sasha151299qwe@gmail.com>
2021-10-12 11:16:09 +0400

    Добавьте файлы проекта.

---
+++ Task_1_1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Task_1_1
+{
+    class Program
+    {
+        static string GetUnicode(ConsoleKeyInfo ch)
+        {
+            String number = Convert.ToString((int)ch.KeyChar, 16);
+            switch (number.Length)
+            {
+                case 0:
+                    return ch.KeyChar + "\t0x0000";
+                case 1:
+                    return (ch.KeyChar + "\t0x000" + number);
+                case 2:
+                    return (ch.KeyChar + "\t0x00" + number);
+                case 3:
+                    return (ch.KeyChar + "\t0x0" + number);
+                default:
+                    return (ch.KeyChar + "\t0x" + number);
+            }
+        }
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo cki;
+
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+            while (true)
+            {
+                Console.WriteLine("Чтобы закончить введите Enter");
+                Console.Write("Введите символ: ");
+                cki = Console.ReadKey(true);
+                if (cki.Key == ConsoleKey.Enter)
+                    return;
+                else
+                    Console.WriteLine(GetUnicode(cki));
+            }
+        }
+    }
+}
---
+++ Task_1_2/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static int ReadInt()
+        {
+            int val;
+            while (!int.TryParse(Console.ReadLine(), out val))
+            {
+                Console.Write("Введите длину массива: ");
+            }
+            return val;
+        }
+        static int[] GetArrayForLenght(int lenght)
+        {
+            int[] array = new int[lenght];
+            var random = new Random();
+            for (int i = 0; i < lenght; i++)
+            {
+                array[i] = random.Next(1, 65536);
+            }
+            return array;
+        }
+
+        static double GetMedian(int[] arr)
+        {
+
+            if (arr.Length % 2 == 1)
+                return arr[arr.Length / 2];
+            else
+                return (double)((double)arr[(arr.Length - 1) / 2] + (double)arr[arr.Length / 2]) / 2;
+        }
+
+        static String GetMode(int[] arr)
+        {
+            int[] helpArray = new int[65536];
+            int maxCount = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                helpArray[arr[i]]++;
+            }
+            for (int i = 0; i < helpArray.Length; i++)
+            {
+                if (helpArray[i] > maxCount)
+                    maxCount = helpArray[i];
+            }
+            StringBuilder sb = new StringBuilder("");
+            for (int i = 0; i < helpArray.Length; i++)
+            {
+                if (helpArray[i] == maxCount)
+                    sb.Append(" " + i);
+            }
+            return sb.ToString();
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Введите длину массива: ");
+            int length = ReadInt();
+            int[] array = new int[length];
+            array = GetArrayForLenght(length);
+            Array.Sort(array);
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Медиана: " + GetMedian(array));
+            Console.WriteLine("Мода или моды: " + GetMode(array));
+
+        }
+    }
+}
